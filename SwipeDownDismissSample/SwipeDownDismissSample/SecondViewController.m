8bd9f0d67737d093cae8441fbd12c6578638ceca
inaba masaya <inabamasaya0506@gmail.com>
2015-11-25 19:54:58 +0900

    コーデイング全て完了

--- SwipeDownDismissSample/SwipeDownDismissSample/SecondViewController.m
+++ SwipeDownDismissSample/SwipeDownDismissSample/SecondViewController.m
@@ -8,30 +8,89 @@
 
 #import "SecondViewController.h"
 
+static const CGFloat kMovePointBorderLine = 100.0f;     // 画面の上からこの値までで指を離した場合は上に戻す
+static const NSTimeInterval kOriginalFrameAnimationSpeed = 0.1f;    // 上に戻す時のアニメーションスピード
+static const NSTimeInterval kDismissAnimationSpeed = 0.3f;      // 下に下げてDismissする時のアニメーションスピード
+
 @interface SecondViewController ()
+@property (weak, nonatomic) IBOutlet UIView *mainView;
 
 @end
 
 @implementation SecondViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // パンジェスチャーを生成してaddする
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureAction:)];
+    [self.mainView addGestureRecognizer:panGesture];
 }
 
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)panGestureAction:(UIPanGestureRecognizer *)recognizer {
+    
+    // 移動量
+    CGPoint movePoint = [recognizer translationInView:self.mainView];
+    
+    // 下に下げる際にx座標が多少横にずれても下と判定する
+    if (movePoint.y > 0 && (-movePoint.y < movePoint.x && movePoint.x < movePoint.y)) { //下に下げた場合
+        self.mainView.frame = CGRectMake(self.mainView.frame.origin.x, movePoint.y, self.mainView.frame.size.width, self.mainView.frame.size.height);
+    } else if (movePoint.y < 0 && self.mainView.frame.origin.y > 0) {   //上に上げた場合
+        // 上に上げた時にすでにy座標が100以下の場合はそれ以上操作させずに元に戻す
+        if (movePoint.y < kMovePointBorderLine) {
+            [self animateOriginalFrame];
+            return;
+        }
+        self.mainView.frame = CGRectMake(self.mainView.frame.origin.x, movePoint.y, self.mainView.frame.size.width, self.mainView.frame.size.height);
+    } else {
+        // 下に下げたが横に移動した等は全て元に戻す
+        [self animateOriginalFrame];
+        return;
+    }
+    
+    // ドラッグが終わり次第場所によってDismissするか元に戻すか判定する
+    if (recognizer.state == UIGestureRecognizerStateEnded) {
+        if (movePoint.y > kMovePointBorderLine) {
+            [self animateDismissViewController];
+        } else {
+            [self animateOriginalFrame];
+        }
+    }
 }
-*/
+
+// 元に戻すアニメーションメソッド
+- (void)animateOriginalFrame {
+    
+    [UIView animateWithDuration:kOriginalFrameAnimationSpeed delay:0.0f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        // 初期値に戻す
+        self.mainView.frame = CGRectMake(self.mainView.frame.origin.x, 0, self.mainView.frame.size.width, self.mainView.frame.size.height);
+    } completion:^(BOOL finished) {
+
+    }];
+}
+
+// ViewControllerを下に下げてからDismissするメソッド
+- (void)animateDismissViewController {
+    
+    [UIView animateWithDuration:kDismissAnimationSpeed delay:0.0f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        // 下に下げる
+        self.mainView.frame = CGRectMake(self.mainView.frame.origin.x, self.mainView.frame.size.height, self.mainView.frame.size.width, self.mainView.frame.size.height);
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:NO completion:nil];
+    }];
+}
+
+// UIViewControllerの背景を透過させる
+- (UIModalPresentationStyle)modalPresentationStyle {
+    
+    return UIModalPresentationOverCurrentContext;
+}
+
 
 @end
